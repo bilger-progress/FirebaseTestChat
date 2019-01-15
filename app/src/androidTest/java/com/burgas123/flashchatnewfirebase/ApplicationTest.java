a148ad28cfe153d099afaf86631f19f02ecfc2f1
bilger-progress <bilger.yahov@progress.com>
2019-01-15 13:25:15 +0200

    Re-factor package name.

--- app/src/androidTest/java/com/burgas123/flashchatnewfirebase/ApplicationTest.java
+++ app/src/androidTest/java/com/burgas123/flashchatnewfirebase/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.burgas123.flashchatnewfirebase;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/burgas123/flashchatnewfirebase/ExampleUnitTest.java
+++ app/src/test/java/com/burgas123/flashchatnewfirebase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.burgas123.flashchatnewfirebase;
 
 import org.junit.Test;
 
